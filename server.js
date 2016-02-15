608de9656ea45334258109e21a1d68d98d0ae343
Rafael J. Rodriguez <rafase282@gmail.com>
2016-02-15 21:46:11 +0000

    Adding access control

--- server.js
+++ server.js
@@ -33,6 +33,19 @@ app.use(bodyParser.json());
 // Use the passport package in our application
 app.use(passport.initialize());
 
+app.all('/*', function(req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  // Set custom headers for CORS
+  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+  if (req.method == 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
+
 // configure the view
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
