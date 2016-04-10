8f229be2672a249c49dd24551eed023559565f1a
Rafael J. Rodriguez <rafase282@gmail.com>
2016-04-10 05:25:42 +0000

    Reduce lines

--- controllers/manga.js
+++ controllers/manga.js
@@ -2,16 +2,12 @@
 // Load required packages
 var Manga = require('../models/manga');
 var dbHelper = require('./dbHelper');
-
-/* Middleware To Output User Location & IP
- * This is disabled by default but it logs user ip and location
- * based on IP when first connecting to the API.
- */
+var noOk = 'No mangas were found!';
+var auth = 'You are not an admin!';
 
 /* Get Welcome Message From Root
  * Displays a welcome message when visiting the root of the API.
- * Accessed at GET /api
- */
+ * Accessed at GET /api */
 exports.getWelcome = function (req, res) {
   var msg = 'Welcome, check the API usage at ' + process.env.APP_URL +
     ', there is nothing to do here.';
@@ -24,35 +20,29 @@ exports.getIndex = function (req, res) {
 };
 
 /* Creates New Manga
- * Returns the manga information.
- * Accessed at POST /api/mangas/:username
- */
+ * Returns the manga information. Accessed at POST /api/mangas/:username */
 exports.postManga = function (req, res) {
   var targetUser = req.params.username.toLowerCase();
   if (req.decoded.sub === process.env.ADMIN ||
     req.decoded.sub === targetUser) {
     var manga = new Manga(); // create a new instance of the Manga model
     manga = dbHelper.updateMangaObj(req, manga);
-      // Call function to save manga
     var msg = req.body.title + ' manga created.';
     dbHelper.objSave(manga, res, msg);
   } else {
-    msg = 'You are not ' + targetUser +
-      ' or an admin!';
+    msg = 'You are not ' + targetUser + ' or an admin!';
     dbHelper.resMsg(res, 403, false, msg, null);
   }
 };
 
 /* Finds Manga By Title
  * Returns the manga information.
- * Accessed at GET /api/mangas/:username/:manga_title
- */
+ * Accessed at GET /api/mangas/:username/:manga_title */
 exports.getManga = function (req, res) {
   var targetUser = req.params.username.toLowerCase();
   var ok = req.params.manga_title + ' found!';
   var noOk = req.params.manga_title + ' not found!';
-  var auth = 'You are not ' + targetUser +
-    ' or an admin!';
+  var auth = 'You are not ' + targetUser + ' or an admin!';
   var obj = {
     title: req.params.manga_title.toLowerCase(),
     username: targetUser
@@ -62,11 +52,9 @@ exports.getManga = function (req, res) {
 
 /* Updates Manga By Title
  * Returns the manga information.
- * Accessed at PUT /api/mangas/:username/:manga_title
- */
+ * Accessed at PUT /api/mangas/:username/:manga_title */
 exports.putManga = function (req, res) {
   var targetUser = req.params.username.toLowerCase();
-  // use our manga model to find the manga we want
   if (req.decoded.sub === process.env.ADMIN ||
     req.decoded.sub === targetUser) {
     Manga.findOne({
@@ -77,22 +65,19 @@ exports.putManga = function (req, res) {
         dbHelper.resMsg(res, 400, false, err, null);
       } else {
         manga = dbHelper.updateMangaObj(req, manga);
-          // update the manga
         var msg = req.params.manga_title + ' manga updated.';
         dbHelper.objSave(manga, res, msg);
       }
     });
   } else {
-    var msg = 'You are not ' + targetUser +
-      ' or an admin!';
+    var msg = 'You are not ' + targetUser + ' or an admin!';
     dbHelper.resMsg(res, 403, false, msg, null);
   }
 };
 
 /* Deletes Manga By Title
  * Returns the manga information.
- * Accessed at DELETE /api/mangas/:username/:manga_title
- */
+ * Accessed at DELETE /api/mangas/:username/:manga_title */
 exports.delManga = function (req, res) {
   var targetUser = req.params.username.toLowerCase();
   var noOk = 'Could not find ' + req.params.manga_title;
@@ -102,71 +87,52 @@ exports.delManga = function (req, res) {
     title: req.params.manga_title.toLowerCase(),
     username: targetUser
   };
-
   dbHelper.delData(req, res, Manga, obj, ok, noOk, auth);
 };
 
 /* Finds All Mangas By User
  * Returns a list of all the mangas the user has.
- * Accessed at GET /api/mangas/:username
- */
+ * Accessed at GET /api/mangas/:username */
 exports.getMangas = function (req, res) {
   var targetUser = req.params.username.toLowerCase();
   var ok = 'Manga List Generated.';
   var noOk = targetUser + ' has not added any mangas yet.';
-  var auth = 'You are not ' + targetUser +
-    ' or an admin!';
+  var auth = 'You are not ' + targetUser + ' or an admin!';
   var userName = req.decoded.sub === process.env.ADMIN ?
     targetUser : req.decoded.sub;
   var obj = {
     username: userName
   };
-
   dbHelper.getData(req, res, Manga, obj, ok, noOk, auth);
 };
 
 /* Finds All Mangas By All Users Via Admin
- * Returns a list of all the mangas for all users.
- * Accessed at GET /api/mangas
- */
+ * Returns a list of all the mangas for all users. Accessed at GET /api/mangas*/
 exports.getAllMangas = function (req, res) {
   var ok = 'Manga List Generated.';
-  var noOk = 'No Mangas has been added yet.';
-  var auth = 'You are not an admin!';
   var obj = {};
-
   dbHelper.getData(req, res, Manga, obj, ok, noOk, auth);
 };
 
 /* Deletes All Mangas For All Users Via Admin
- * Returns the manga information.
- * Accessed at DELETE /api/mangas/
- */
+ * Returns the manga information. Accessed at DELETE /api/mangas/ */
 exports.delMangas = function (req, res) {
   var ok = 'Successfully deleted all mangas.';
-  var noOk = 'No mangas were found!';
-  var auth = 'You are not an admin!';
   var obj = {
     username: {
       $ne: process.env.ADMIN.toLowerCase()
     }
   };
-
   dbHelper.delData(req, res, Manga, obj, ok, noOk, auth);
 };
 
 /* Deletes All Mangas For All Users Via Admin
- * Returns the manga information.
- * Accessed at DELETE /api/mangas/:username
- */
+ * Returns the manga information. Accessed at DELETE /api/mangas/:username */
 exports.delUserMangas = function (req, res) {
   var targetUser = req.params.username.toLowerCase();
-  var noOk = 'No mangas were found!';
   var ok = 'Successfully deleted all user mangas.';
-  var auth = 'You are not an admin!';
   var obj = {
     username: targetUser
   };
-
   dbHelper.delData(req, res, Manga, obj, ok, noOk, auth);
 };
